078e61b5876fbd265fef5ee16235b965842b7f35
keshavbansal5766 <keshav22.dlp@gmail.com>
2025-11-04 00:28:45 +0530

    Setting up project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+     <div className="min-h-screen flex items-center justify-center bg-green-600 text-white text-4xl font-bold">
+      Tailwind Working ✅
+    </div>
+  );
+};
+
+export default App;

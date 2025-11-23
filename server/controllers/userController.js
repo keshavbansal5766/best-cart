d61f87eb834e69e57cc6574d4cd80993696d3f5b
keshavbansal5766 <keshav22.dlp@gmail.com>
2025-11-24 03:01:23 +0530

    creating register function and make its route and testing it

---
+++ server/controllers/userController.js
@@ -0,0 +1,40 @@
+import bcrypt from "bcryptjs";
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+
+// Register: /api/user/register
+export const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    if (!email || !name || !password) {
+      return res.json({ success: false, message: "Missing Details" });
+    }
+
+    const existingUSer = await User.findOne({ email });
+    if (existingUSer)
+      return res.json({ success: false, message: "User already exists" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({ name, email, password: hashedPassword });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true, // Prevent Javascript to access cookie
+      secure: process.env.NODE_ENV === "production", // Use secure cookies in production
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict", // CSRF protection
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    return res.json({
+      success: true,
+      user: { email: user.email, name: user.name },
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
+
 import cookieParser from "cookie-parser";
 import express from "express";
 import cors from "cors";
 import connectDB from "./configs/db.js";
 import "dotenv/config";
+import userRouter from "./routes/userRouter.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -20,6 +22,8 @@ app.use(cors({ origin: allowedOrigins, credentials: true }));
 app.get("/", (req, res) => {
   res.send("API is Working");
 });
+
+app.use("/api/user", userRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);

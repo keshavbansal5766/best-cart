6a864fd14d7c6c4750e792c95575ebf1455b2990
keshavbansal5766 <keshav22.dlp@gmail.com>
2025-11-26 02:42:35 +0530

    Creating sellerLogin, isSellerAuth and authSeller

---
+++ server/controllers/sellerLogin.js
@@ -0,0 +1,42 @@
+import jwt from "jsonwebtoken";
+
+//Login Seller : /api/seller/login
+
+export const sellerLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (
+      email === process.env.SELLER_EMAIL &&
+      password === process.env.SELLER_PASSWORD
+    ) {
+      const token = jwt.sign({ email }, process.env.JWT_SECRET, {
+        expiresIn: "7d",
+      });
+
+      res.cookie("sellerToken", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        maxAge: 7 * 24 * 60 * 6 * 1000,
+      });
+
+      return res.json({ success: true, message: "Logged In" });
+    } else {
+      return res.json({ success: false, message: "Invalid Credentials" });
+    }
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// Seller Auth : /api/seller/is-auth
+
+export const isSellerAuth = async (req, res) => {
+  try {
+    return res.json({ success: true });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message`` });
+  }
+};
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,7 +1,15 @@
 import express from "express";
-import { Login, register } from "../controllers/userController.js";
+import {
+  isAuth,
+  Login,
+  logout,
+  register,
+} from "../controllers/userController.js";
+import authUser from "../middlewares/authUser.js";
 
 const userRouter = express.Router();
 userRouter.post("/register", register);
 userRouter.post("/login", Login);
+userRouter.get("/is-auth", authUser, isAuth);
+userRouter.get("/logout", logout);
 export default userRouter;

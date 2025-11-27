4fce42422bdd0a64c6d3424e1831aeff807658ea
keshavbansal5766 <keshav22.dlp@gmail.com>
2025-11-28 01:38:19 +0530

    Creating product add api

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,2 +1,28 @@
+import { v2 as cloudinary } from "cloudinary";
+import Product from "../models/Product.js";
+
 // Add Product: /api/product/add
-export const addProduct = async (req, res) => {};
+export const addProduct = async (req, res) => {
+  try {
+    let productData = JSON.parse(req.body.productData);
+
+    let images = req.files;
+
+    let imagesUrl = await Promise.all(
+      images.map(async (item) => {
+        let result = await cloudinary.uploader.upload(item.path, {
+          resource_type: "image",
+        });
+
+        return result.secure_url;
+      })
+    );
+
+    await Product.create({ ...productData, image: imagesUrl });
+
+    return res.json({ success: true, message: "Product Added" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};

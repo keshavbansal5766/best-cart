44291ad949dcb80856133c9e8249680098e22e50
keshavbansal5766 <keshav22.dlp@gmail.com>
2025-11-27 23:33:55 +0530

    Creating Seller logout

--- server/controllers/sellerLogin.js
+++ server/controllers/sellerLogin.js
@@ -40,3 +40,20 @@ export const isSellerAuth = async (req, res) => {
     res.json({ success: false, message: error.message`` });
   }
 };
+
+// Logout Seller: /api/seller/logout
+
+export const sellerLogout = (req, res) => {
+  try {
+    res.clearCookie("sellerToken", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    });
+
+    return res.json({ success: true, message: "Logged Out" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};

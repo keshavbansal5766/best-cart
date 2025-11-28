f37bb8a85b31c52b23922e93eb36d610385d027e
keshavbansal5766 <keshav22.dlp@gmail.com>
2025-11-28 14:13:20 +0530

    Creating getProductList, getProductByID, productInStock fucntions

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -26,3 +26,39 @@ export const addProduct = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+
+// Get Product : /api/product/list
+export const productList = async (req, res) => {
+  try {
+    const products = await Product.find({});
+
+    return res.json({ success: true, products });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// Get Single Product : /api/product/id
+export const productById = async (req, res) => {
+  try {
+    const { id } = req.body;
+    const product = await Product.findById(id);
+    res.json({ success: true, product });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// Change Product inStock : /api/product/stock
+export const changeStock = async (req, res) => {
+  try {
+    const { id, inStock } = req.body;
+    await Product.findByIdAndUpdate(id, { inStock });
+    res.json({ success: true, message: "Stock Updated" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
--- server/models/Product.js
+++ server/models/Product.js
@@ -14,6 +14,6 @@ const productSchema = new mongoose.Schema(
 );
 
 const Product =
-  mongoose.model.product || mongoose.model("product", productSchema);
+  mongoose.models.product || mongoose.model("product", productSchema);
 
 export default Product;

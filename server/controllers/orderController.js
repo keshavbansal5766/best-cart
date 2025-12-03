5cc1b6e093fa77e344548fa8b6c5ea9d832dbdbc
keshavbansal5766 <keshav22.dlp@gmail.com>
2025-12-04 03:10:49 +0530

    fix bug

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -113,7 +113,7 @@ export const stripeWebhooks = async (request, response) => {
   let event;
 
   try {
-    event = stripeInstance.webhooks.construction(
+    event = stripeInstance.webhooks.constructEvent(
       request.body,
       sig,
       process.env.STRIPE_WEBHOOK_SECRET

b9590f0ea6bf99acda393032fe25c5a374fbd760
keshavbansal5766 <keshav22.dlp@gmail.com>
2026-02-18 02:04:06 +0530

    fixing order component

--- client/src/pages/seller/Orders.jsx
+++ client/src/pages/seller/Orders.jsx
@@ -45,7 +45,7 @@ const Orders = () => {
                 {order.items.map((item, index) => (
                   <div key={index} className="flex flex-col">
                     <p className="font-medium">
-                      {item.product.name}{" "}
+                      {item.product?.name}{" "}
                       <span className="text-primary">x {item.quantity}</span>
                     </p>
                   </div>
